266a51241d7ccb047bb1900859016dbea4313858
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-18 14:40:55 +0530

    Add mappingLengths.js file

---
+++ mappingLengths.js
@@ -0,0 +1,15 @@
+function computeLengthsArray(elements)
+{
+    let listOfLengths = []
+    for(let index = 0; index < elements.length; index++)
+    {
+        listOfLengths.push(elements[index].length);
+    }
+    return listOfLengths;
+}
+function printLengthsOfElements()
+{
+    console.log("Array of lengths of elements is : " + lengthsList);
+}
+lengthsList = computeLengthsArray(["Mahathi", "Java", "Python", "Github", "Lengths"]);
+printLengthsOfElements()
