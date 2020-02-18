c2fd896a52482ee070187be464f1c49c8fca13f7
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-18 15:12:13 +0530

    Add belowThreshold.js file

---
+++ belowThreshold.js
@@ -0,0 +1,22 @@
+function isBelowThreshold(element, threshold)
+{
+    if(element < threshold)
+        return true;
+    return false;
+}
+function getBelowThresholdCount(elements, threshold)
+{
+    let countBelowThreshold = 0;
+    for(let index = 0; index < elements.length; index++)
+    {
+        if(isBelowThreshold(elements[index], threshold))
+            countBelowThreshold++;
+    }
+    return countBelowThreshold;
+}
+function printCount()
+{
+    console.log("Number of elemets below threshold is : " + belowThresholdCount)
+}
+var belowThresholdCount = getBelowThresholdCount([10,5,24,32,19,8,29],11);
+printCount();
