daa4266531e568e85488f17ca39ea08bbdb74670
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-18 12:14:15 +0530

    Add sumOfListElements.js

---
+++ sumOfListElements.js
@@ -0,0 +1,15 @@
+function addingListElements(elements)
+{
+    let sum=0;
+    for(let index=0; index < elements.length; index++)
+    {
+        sum = sum + elements[index];
+    }
+    return sum;
+}
+function printSum()
+{
+    console.log("Sum of elements in the list is : "+sumOfNumbers);
+}
+var sumOfNumbers = addingListElements([5,9,10,12]);
+printSum();
