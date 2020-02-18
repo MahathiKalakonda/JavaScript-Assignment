68e4a41d4bc0db14b4a45c8f6f611ccc23ec159e
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-18 15:34:02 +0530

    Add indexOf.js file

---
+++ indexOf.js
@@ -0,0 +1,15 @@
+function getPositionOf(list, keyElement)
+{
+    for(let index = 0; index < list.length; index++)
+    {
+        if(keyElement == list[index])
+            return index;
+    }
+    return -1;
+}
+function printPosition()
+{
+    console.log("The index of the element in the list is : " + position);
+}
+var position = getPositionOf([10,19,13,12,9,13,24],13);
+printPosition();
