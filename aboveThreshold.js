5c02b1db71c0e53223a15bd3a157891ed9a21c40
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-18 15:08:13 +0530

    Add aboveThreshold.js file

---
+++ aboveThreshold.js
@@ -0,0 +1,22 @@
+function isAboveThreshold(element, threshold)
+{
+    if(element > threshold)
+        return true;
+    return false;
+}
+function getAboveThresholdCount(elements, threshold)
+{
+    let countAboveThreshold = 0;
+    for(let index = 0; index < elements.length; index++)
+    {
+        if(isAboveThreshold(elements[index], threshold))
+            countAboveThreshold++;
+    }
+    return countAboveThreshold;
+}
+function printCount()
+{
+    console.log("Number of elemets above threshold is : " + aboveThresholdCount)
+}
+var aboveThresholdCount = getAboveThresholdCount([10,5,24,32,19,8,29],11);
+printCount();
