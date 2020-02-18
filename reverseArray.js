63cb30dc2f89e37dab2b9363472d8ce99999a22c
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-18 15:19:56 +0530

    Add reverseArray.js file

---
+++ reverseArray.js
@@ -0,0 +1,15 @@
+function getReverseArray(elements)
+{
+    let reversedList = [];
+    for(let index = 0; index < elements.length; index++)
+    {
+        reversedList.unshift(elements[index]);
+    }
+    return reversedList;
+}
+function printReverseList()
+{
+    console.log("The reversed array is : " + reverseList)
+}
+var reverseList = getReverseArray([90,12,19,2,34,91,10]);
+printReverseList();
