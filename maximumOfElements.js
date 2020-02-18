5b204e6a9f115420855a54c97594c125ee94524f
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-18 14:24:19 +0530

    Add minimumOfList.js file

--- maximumOfElements.js
+++ maximumOfElements.js
@@ -17,5 +17,5 @@ function printMaximum()
 {
     console.log("Maximum element is : " + maximumOfList);
 }
-var maximumOfList = getMaximumElement([2,10,'abc',202,19,25,36]);
+var maximumOfList = getMaximumElement([2,10,202,19,25,36]);
 printMaximum()
---
+++ minimumOfList.js
@@ -0,0 +1,21 @@
+function getMinimumOf2(number1,number2)
+{
+    if(number1 < number2)
+        return number1;
+    return number2;
+}
+function getMinimumElement(elements)
+{
+    let minimumNumber = elements[0];
+    for(let index = 1; index < elements.length; index++)
+    {
+        minimumNumber = getMinimumOf2(minimumNumber, elements[index]);
+    }
+    return minimumNumber;
+}
+function printMinimum()
+{
+    console.log("Minimum element is : " + minimumOfList);
+}
+var minimumOfList = getMinimumElement([2,10,202,19,25,36]);
+printMinimum()
