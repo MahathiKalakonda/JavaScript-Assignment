34509f8e3330cbe6e7c6fad2354fc2848f051c08
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-21 13:18:48 +0530

    Add map function to mappingLengths.js

--- mappingLengths.js
+++ mappingLengths.js
@@ -13,5 +13,9 @@ function printLengthsOfElements()
     console.log("Array of lengths of elements is: " + lengthsList);
 }
 
-lengthsList = computeLengthsArray(["Mahathi", "Java", "Python", "Github", "Lengths"]);
+array = ["Mahathi", "Java", "Python", "Github", "Lengths"]
+lengthsList = computeLengthsArray(array);
 printLengthsOfElements()
+
+var elementsLengths = array.map(element => element.length);
+console.log("Array of lengths of elements using map function  is : " + elementsLengths);
