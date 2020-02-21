f8452ef148250b65dfbbd7240d0a9b2ac0a81297
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-21 12:39:34 +0530

    Add reduce function to minimumOfList.js

--- minimumOfList.js
+++ minimumOfList.js
@@ -10,7 +10,7 @@ function getMinimumElement(elements)
     let minimumNumber = elements[0];
     for(let index = 1; index < elements.length; index++)
     {
-        minimumNumber = getMinimumOf2(minimumNumber, elements[index]);
+        minimumNumber = getMinimum(minimumNumber, elements[index]);
     }
     return minimumNumber;
 }
@@ -20,5 +20,9 @@ function printMinimum()
     console.log("Minimum element is: " + minimumOfList);
 }
 
-var minimumOfList = getMinimumElement([2,10,202,19,25,36]);
-printMinimum()
+array = [2, 10, 202, 19, 25, 36];
+var minimumOfList = getMinimumElement(array);
+printMinimum();
+
+var minimumOfArray = array.reduce((accumulator,currentValue)=> accumulator < currentValue ? accumulator : currentValue);
+console.log("Minimum of list using reduce function is : " + minimumOfArray);
