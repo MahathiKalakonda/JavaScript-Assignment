20672a5e85ca1b32048ef8f59f60f1bd82050532
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-21 12:36:15 +0530

    Add reduce function to maximumOfElements.js

--- maximumOfElements.js
+++ maximumOfElements.js
@@ -10,7 +10,7 @@ function getMaximumElement(elements)
     let maximumNumber = elements[0];
     for(let index = 1; index < elements.length; index++)
     {
-        maximumNumber = getMaximumOf2(maximumNumber, elements[index]);
+        maximumNumber = getMaximum(maximumNumber, elements[index]);
     }
     return maximumNumber;
 }
@@ -20,5 +20,9 @@ function printMaximum()
     console.log("Maximum element is: " + maximumOfList);
 }
 
-var maximumOfList = getMaximumElement([2,10,202,19,25,36]);
-printMaximum()
+array = [2, 10, 202, 19, 25, 36];
+var maximumOfList = getMaximumElement(array);
+printMaximum();
+
+var maximumOfArray = array.reduce((accumulator,currentValue)=> accumulator > currentValue ? accumulator : currentValue);
+console.log("Maximum of list using reduce function is : " + maximumOfArray);
