7578b99e308a6f61192c05b06d269e28a619119d
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-21 13:56:04 +0530

    Add map function to reverseArray.js

--- reverseArray.js
+++ reverseArray.js
@@ -10,8 +10,16 @@ function getReverseArray(elements)
 
 function printReverseList()
 {
-    console.log("The reversed array is: " + reverseList)
+    console.log("The reversed array is: " + reverseList);
 }
 
-var reverseList = getReverseArray([90,12,19,2,34,91,10]);
+var array = [90, 12, 19, 2, 34, 91, 10];
+var reverseList = getReverseArray(array);
 printReverseList();
+
+var reverseArray = array.map(function (element, currentIndex, array)
+{
+    let index = array.length - currentIndex - 1;
+    return array[index];
+})
+console.log("The reversed list using map is : " + reverseArray);
